7a12b64272658a7500dcee0aba78d2902c74a2ee
flackdl <flackattack@gmail.com>
2019-11-30 18:19:27 -0600

    remove logging

--- static/app.js
+++ static/app.js
@@ -6,7 +6,7 @@ let Person = function(name, email) {
     this.errors = [];
 };
 Person.prototype.has_error = function (name) {
-  return _.indexOf(this.errors, name) != -1;
+    return _.indexOf(this.errors, name) != -1;
 };
 
 let Match = function(buyer, recipient) {
@@ -15,13 +15,13 @@ let Match = function(buyer, recipient) {
 };
 
 let init_participants = function() {
-  // start off with a list of blank participants
-  let participants = [];
-  _.forEach(_.range(0, 4), function(v, i) {
-      participants.push(new Person());
-  });
-  
-  return participants;
+    // start off with a list of blank participants
+    let participants = [];
+    _.forEach(_.range(0, 4), function(v, i) {
+        participants.push(new Person());
+    });
+
+    return participants;
 };
 
 let validateEmail = function (email) {
@@ -30,187 +30,184 @@ let validateEmail = function (email) {
 };
 
 let assign = function(participants) {
-  let assigned = [];
-  
-  _.forEach(participants.slice(), function(participant) {
-    
-    // remove self, assigned and exceptions
-    let available_recipients = _.filter(participants, function(p) {
-      // self
-      if (p.email === participant.email) {
-          console.log('self', participant);
-        return false; 
-      }
-      // assigned
-      if (_.find(assigned, function(a) {return a.recipient.email === p.email})) {
-          console.log('assigned', participant);
-        return false;
-      }
-      // exception
-      return !_.find(participant.exceptions, function(exception) {
-        // exceptions are in the select "options" format so check "value"
-        return exception.value === p.email;
-      });
+    let assigned = [];
+
+    _.forEach(participants.slice(), function(participant) {
+
+        // remove self, assigned and exceptions
+        let available_recipients = _.filter(participants, function(p) {
+            // self
+            if (p.email === participant.email) {
+                return false;
+            }
+            // assigned
+            if (_.find(assigned, function(a) {return a.recipient.email === p.email})) {
+                return false;
+            }
+            // exception
+            return !_.find(participant.exceptions, function(exception) {
+                // exceptions are in the select "options" format so check "value"
+                return exception.value === p.email;
+            });
+        });
+
+        // choose a random participant
+        if (available_recipients.length) {
+            assigned.push(new Match(
+                participant, available_recipients[_.random(available_recipients.length - 1)]));
+        } else {
+            throw 'Failed matching';
+        }
     });
-    
-    // choose a random participant
-    if (available_recipients.length) {
-      assigned.push(new Match(
-        participant, available_recipients[_.random(available_recipients.length - 1)]));
-    } else {
-      throw 'Failed matching';
-    }
-  });
-  
-  return assigned;
+
+    return assigned;
 };
 
 Vue.component('v-select', VueSelect.VueSelect);
 
 let app = new Vue({
-  el: '#secret-santa',
-  delimiters: ['{$', '$}'],
-  data: {
-    participants: init_participants(),
-    assigned: [],
-    error: null,
-    secret: false,
-    email_sent: false,
-    participant_exceptions: [],
-  },
-  methods: {
-    addParticipant: function() {
-        console.log(this.participants);
-      this.participants.push(new Person());
-      this.assigned = [];
+    el: '#secret-santa',
+    delimiters: ['{$', '$}'],
+    data: {
+        participants: init_participants(),
+        assigned: [],
+        error: null,
+        secret: false,
+        email_sent: false,
+        participant_exceptions: [],
     },
-    importParticipants: function() {
-        CSV.fetch({
-                file: document.getElementById('csv').files[0],
-            }
-        ).then((dataset) => {
-            this.participants = [];
-            _.forEach(dataset.records, (row) => {
-                // name and email are required
-                if (!row[0] || !row[1]) {
-                    return;
+    methods: {
+        addParticipant: function() {
+            this.participants.push(new Person());
+            this.assigned = [];
+        },
+        importParticipants: function() {
+            CSV.fetch({
+                    file: document.getElementById('csv').files[0],
                 }
-                let participant = new Person(row[0], row[1]);
-                // has exclusions
-                if (row.length > 2) {
-                    for (let i = 2; i < 100; i++) {
-                        if (row[i]) {
-                            participant.exception_emails.push(row[i]);
+            ).then((dataset) => {
+                this.participants = [];
+                _.forEach(dataset.records, (row) => {
+                    // name and email are required
+                    if (!row[0] || !row[1]) {
+                        return;
+                    }
+                    let participant = new Person(row[0], row[1]);
+                    // has exclusions
+                    if (row.length > 2) {
+                        for (let i = 2; i < 100; i++) {
+                            if (row[i]) {
+                                participant.exception_emails.push(row[i]);
+                            }
                         }
                     }
-                }
-                this.participants.push(participant);
-            });
-            _.forEach(this.participants, (participant) => {
-                _.forEach(participant.exception_emails, (email) => {
-                    const match = _.find(this.participants, (p) => {
-                        return p.email === email;
-                    });
-                    if (match) {
-                        participant.exceptions.push({
-                            label: match.name,
-                            value: match.email,
+                    this.participants.push(participant);
+                });
+                _.forEach(this.participants, (participant) => {
+                    _.forEach(participant.exception_emails, (email) => {
+                        const match = _.find(this.participants, (p) => {
+                            return p.email === email;
                         });
-                    }
+                        if (match) {
+                            participant.exceptions.push({
+                                label: match.name,
+                                value: match.email,
+                            });
+                        }
+                    });
                 });
             });
-        });
-    },
-    removeParticipant: function(i) {
-      this.participants.splice(i, 1);
-      this.assigned = [];
-    },
-    assign: function(e) {
-        this.email_sent = false;
-        this.error = null;
-      
-        // remove blank entries
-        this.participants = _.filter(this.participants, function(p) {return p.name || p.email});
-        if (!this.participants.length) {
-         this.error = 'There are no valid participants to assign'; 
-         return;
-        }
-        
-        if (!this.validate()) {
-          return;
-        }
-        
-        // reset assignments
-        this.assigned = [];
-        
-        // use a reasonable attempt limit
-        let i = 0;
-        while(!this.assigned.length && i++ <= 100) {
-          this.assigned = [];
-            try {
-                this.assigned = assign(this.participants);
-            } catch (e) {
+        },
+        removeParticipant: function(i) {
+            this.participants.splice(i, 1);
+            this.assigned = [];
+        },
+        assign: function(e) {
+            this.email_sent = false;
+            this.error = null;
+
+            // remove blank entries
+            this.participants = _.filter(this.participants, function(p) {return p.name || p.email});
+            if (!this.participants.length) {
+                this.error = 'There are no valid participants to assign';
+                return;
             }
-        }
-        if (!this.assigned.length) {
-          this.error = 'There are too many exclusions to assign correctly';
+
+            if (!this.validate()) {
+                return;
+            }
+
+            // reset assignments
+            this.assigned = [];
+
+            // use a reasonable attempt limit
+            let i = 0;
+            while(!this.assigned.length && i++ <= 100) {
+                this.assigned = [];
+                try {
+                    this.assigned = assign(this.participants);
+                } catch (e) {
+                }
+            }
+            if (!this.assigned.length) {
+                this.error = 'There are too many exclusions to assign correctly';
+            }
+        },
+        exceptions: function (participant) {
+            // remove the participant as an exception option
+            let exceptions = _.filter(this.participants, function(p) {return p.email !== participant.email});
+            exceptions.unshift();
+            return exceptions;
+        },
+        exceptions_options: function (participant) {
+            if (!participant.email) {
+                return [];
+            }
+            return _.map(_.filter(this.exceptions(participant), function(p) { return p.email; }), function(exception) {
+                return {
+                    label: exception.name,
+                    value: exception.email,
+                };
+            });
+        },
+        is_valid: function () {
+            let valid = true;
+            _.forEach(this.participants.slice(), function(participant, index) {
+                if (participant.errors.length) {
+                    valid = false;
+                    return;
+                }
+            });
+            return valid;
+        },
+        validate: function(participant) {
+            let participants = participant ? [participant] : this.participants;
+            _.forEach(participants.slice(), function(participant, index) {
+                if (!participant.name || !participant.name.length) {
+                    participant.errors.push('name');
+                } else {
+                    participant.errors = _.filter(participant.errors, function(e) {
+                        return e != 'name';
+                    });
+                }
+                if (!validateEmail(participant.email)) {
+                    participant.errors.push('email');
+                } else {
+                    participant.errors = _.filter(participant.errors, function(e) {
+                        return e != 'email';
+                    });
+                }
+            });
+            return this.is_valid();
+        },
+        send_emails: function() {
+            let app = this;
+            this.$http.post('/send-emails', {'assignments': this.assigned}, {'headers': {'content-type': 'application/json'}}).then((response) => {
+                console.log('success', response);
+                app.email_sent = true;
+            }, (response) => {
+                console.log('failure', response);
+            });
         }
     },
-    exceptions: function (participant) {
-      // remove the participant as an exception option
-      let exceptions = _.filter(this.participants, function(p) {return p.email !== participant.email});
-      exceptions.unshift();
-      return exceptions;
-    },
-    exceptions_options: function (participant) {
-      if (!participant.email) {
-        return [];
-      }
-      return _.map(_.filter(this.exceptions(participant), function(p) { return p.email; }), function(exception) {
-        return {
-          label: exception.name,
-          value: exception.email,
-        };
-      });
-    },
-    is_valid: function () {
-      let valid = true;
-      _.forEach(this.participants.slice(), function(participant, index) {
-        if (participant.errors.length) {
-          valid = false;
-          return;
-        }
-      });
-      return valid;
-    },
-    validate: function(participant) {
-      let participants = participant ? [participant] : this.participants;
-      _.forEach(participants.slice(), function(participant, index) {
-        if (!participant.name || !participant.name.length) {
-          participant.errors.push('name');
-        } else {
-          participant.errors = _.filter(participant.errors, function(e) {
-            return e != 'name';
-          });
-        }
-        if (!validateEmail(participant.email)) {
-          participant.errors.push('email');
-        } else {
-          participant.errors = _.filter(participant.errors, function(e) {
-            return e != 'email';
-          });
-        }
-      });
-      return this.is_valid();
-    },
-    send_emails: function() {
-      let app = this;
-      this.$http.post('/send-emails', {'assignments': this.assigned}, {'headers': {'content-type': 'application/json'}}).then((response) => {
-        console.log('success', response);
-        app.email_sent = true;
-      }, (response) => {
-        console.log('failure', response);
-      });
-    }
-  },
 });
