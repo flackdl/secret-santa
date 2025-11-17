d5dac70793610e71bdda475a55e1312536bb6379
danny <flackattack@gmail.com>
2025-11-17 13:06:10 -0600

    switch from sendgrid to mailersend

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 import os
+from dotenv import load_dotenv
 from flask import Flask, request, render_template, jsonify, send_from_directory
-import sendgrid
-from sendgrid.helpers.mail import Email, To, Content, Mail
+from mailersend import MailerSendClient, EmailBuilder
 
+# load env variables
+load_dotenv()
+
+# set up the mailer-send client
+ms = MailerSendClient()
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -16,40 +21,50 @@ def download_file(filename):
 def index():
     return render_template('index.html')
 
-    
+
 @app.route('/send-emails', methods=['POST'])
 def send_emails():
     try:
         data = request.get_json()
     except:
         return jsonify({'success': False}), 500
-        
-    assignments = data.get('assignments', []) or []
-    
-    app.logger.info(assignments)
 
-    sg_client = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API_KEY'))
+    assignments = data.get('assignments', []) or []
+
+    app.logger.info(assignments)
 
     for assignment in assignments:
 
+        subject = "Ssshhh...{}, this is your Secret Santa recipient".format(assignment['buyer']['name'])
+        content = "Your Secret Santa recipient is %s!" % assignment['recipient']['name']
+
         app.logger.info('%s buys for %s' % (assignment['buyer']['email'], assignment['recipient']['name']))
 
-        from_email = Email("secret-santa@eerieemu.com")
-        subject = "Ssshh...{}, this is your Secret Santa recipient".format(assignment['buyer']['name'])
-        to_email = To(assignment['buyer']['email'])
-        content = Content("text/plain", "Your Secret Santa recipient is %s!" % assignment['recipient']['name'])
-        mail = Mail(from_email, to_email, subject, content)
-        response = sg_client.client.mail.send.post(request_body=mail.get())
+        try:
+            email = (
+                EmailBuilder()
+                .from_email("secret-santa@test-68zxl2707634j905.mlsender.net", "Secret Santa")
+                .to_many([{"email": assignment['buyer']['email'], "name": assignment['buyer']['name']}])
+                .subject(subject)
+                .html(content)
+                .text(content)
+                .build()
+            )
 
-        app.logger.info('%s, %s' % (assignment['buyer']['email'], response.status_code))
-        
+            response = ms.emails.send(email)
+
+            if response.status_code < 200 or response.status_code >= 300:
+                raise Exception(response.body)
+
+            print(f"successfully sent email to {assignment['buyer']['email']}")
+
         # failure
-        if response.status_code < 200 or response.status_code >= 300:
-            app.logger.info('failed')
+        except Exception as e:
+            app.logger.exception(e)
             return jsonify({'success': False}), 500
-            
+
     return jsonify({'success': True})
-              
+
 
 app.config['DEBUG'] = True
 app.secret_key = os.getenv('app_secret', 'sshh')
